6dceed282cf877d83009c195b00d52e59f858bb4
Yigit Gultekin <ygtgltkn1@gmail.com>
2023-11-27 13:03:40 +0300

    Added weapons and shooting to Huwaina scene

---
+++ Assets/Scripts/BulletShoot.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletShoot : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform firePoint;
+    public float bulletSpeed = 1000f; // Mermi hızı, bu değeri ihtiyacınıza göre ayarlayabilirsiniz
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            FireBullet();
+        }
+    }
+
+    void FireBullet()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        Rigidbody rb = bullet.GetComponent<Rigidbody>();
+        if (rb != null)
+        {
+            rb.AddForce(firePoint.forward * bulletSpeed);
+        }
+    }
+}
