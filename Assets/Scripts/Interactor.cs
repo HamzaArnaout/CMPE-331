8aa21246edd34f61dd374040c89c48711b336f03
Twerkonmyham <hamzamurad92@gmail.com>
2023-12-16 23:25:41 +0300

    extra

--- Assets/Scripts/Interactor.cs
+++ Assets/Scripts/Interactor.cs
@@ -1,12 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
-using TMPro.EditorUtilities;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
-using UnityEngine.UI;
-using UnityEngine.VFX;
 
 interface IInteractable
 {
